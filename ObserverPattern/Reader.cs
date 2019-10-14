09d18d46a19cb8bc996e9f15faf559ba76ed006f
PhilipYordanov <fjordanov1@gmail.com>
2019-10-14 18:40:23 +0300

    - simple implementation of the Observer pattern

---
+++ ObserverPattern/Reader.cs
@@ -0,0 +1,15 @@
+﻿namespace ObserverPattern
+{
+    using Interfaces;
+    using System;
+
+    public class Reader : IObserver
+    {
+        private Author _author;
+
+        public Reader(Author author) => this._author = author;
+
+        public void Update() => Console.WriteLine(this._author.GetState());
+    }
+}
+
